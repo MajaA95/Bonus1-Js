2f8a62c394f06d6133f95ee34ca18f0c0264315a
Maja <56198907+MajaA95@users.noreply.github.com>
2022-12-06 00:40:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//50 MPG
+
+var km100ToMil = 62.1371
+var litPerMil= 0.07570
+var litPerKm100 = km100ToMil * litPerMil
+
+console.log(litPerKm100)
+
+//25 MPG
+var km100ToMil = 62.1371
+var litPerMil= 0.151416
+var litPerKm100 = km100ToMil * litPerMil
+
+console.log(litPerKm100)
+
+
+//12L/100km
+
+var lit12ToGalon = 3.170064
+var km100ToMil = 62.1371
+var mpg = km100ToMil / lit12ToGalon
+
+console.log (mpg)
+
+
+
+
+
